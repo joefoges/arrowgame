697e6f0ca449e160b81937bf16da3d83b013a84c
joefoges <steams_knock.7t@icloud.com>
2025-06-16 19:11:58 -0400

    Update Arrow1.js

--- Arrow1/Arrow1.js
+++ Arrow1/Arrow1.js
@@ -1,76 +1,28 @@
-/* eslint-disable require-yield, eqeqeq */
-
-import {
-  Sprite,
-  Trigger,
-  Watcher,
-  Costume,
-  Color,
-  Sound,
-} from "https://unpkg.com/leopard@^1/dist/index.esm.js";
+import { Sprite, Trigger, Script, Costume, Sound } from "leopard";
 
 export default class Arrow1 extends Sprite {
   constructor(...args) {
     super(...args);
 
-    this.costumes = [
-      new Costume("arrow1-a", "./Arrow1/costumes/arrow1-a.svg", {
-        x: 28.14483903477199,
-        y: 23.163332787810276,
-      }),
-    ];
-
-    this.sounds = [new Sound("pop", "./Arrow1/sounds/pop.wav")];
+    this.costumes = [new Costume("arrow", "./Arrow1/costumes/arrow.svg", { x: 0, y: 0 })];
 
     this.triggers = [
-      new Trigger(Trigger.GREEN_FLAG, this.whenGreenFlagClicked),
-      new Trigger(Trigger.GREEN_FLAG, this.whenGreenFlagClicked2),
-      new Trigger(Trigger.CLONE_START, this.startAsClone),
-      new Trigger(Trigger.CLONE_START, this.startAsClone2),
+      new Trigger(Trigger.KEY_PRESSED, { key: "space" }, this.shootArrow),
+      new Trigger(Trigger.CLONE_START, this.flyArrow)
     ];
   }
 
-  *whenGreenFlagClicked() {
-    this.visible = false;
-    while (true) {
-      this.goto(this.sprites["AppleCatcher"].x, this.sprites["AppleCatcher"].y);
-      this.direction = this.radToScratch(
-        Math.atan2(this.mouse.y - this.y, this.mouse.x - this.x)
-      );
-      yield;
-    }
+  shootArrow() {
+    this.createClone();
   }
 
-  *whenGreenFlagClicked2() {
-    while (true) {
-      if (this.mouse.down) {
-        this.createClone();
-      }
-      yield;
-    }
-  }
-
-  *startAsClone() {
+  flyArrow() {
+    this.goto(this.stage.vars.launcherX, this.stage.vars.launcherY); // Adjust if needed
     this.visible = true;
-    while (
-      !(
-        this.touching("edge") ||
-        this.touching(this.sprites["Apple"].andClones())
-      )
-    ) {
+    while (!this.touching("edge")) {
       this.move(10);
-      yield;
+      this.yield();
     }
-    yield* this.wait(1);
     this.deleteThisClone();
-  }
-
-  *startAsClone2() {
-    while (true) {
-      if (this.touching(this.sprites["Apple"].andClones())) {
-        this.say("you win");
-      }
-      yield;
-    }
   }
 }
