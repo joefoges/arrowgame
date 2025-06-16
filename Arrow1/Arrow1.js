8c3bbed9e1a9fcdab13dcf3c36ff20c78343a64c
joefoges <steams_knock.7t@icloud.com>
2025-06-16 19:14:16 -0400

    Update Arrow1.js

--- Arrow1/Arrow1.js
+++ Arrow1/Arrow1.js
@@ -1,28 +1,36 @@
-import { Sprite, Trigger, Script, Costume, Sound } from "leopard";
+import { Sprite, Trigger, Script, Costume } from "leopard";
 
 export default class Arrow1 extends Sprite {
   constructor(...args) {
     super(...args);
 
-    this.costumes = [new Costume("arrow", "./Arrow1/costumes/arrow.svg", { x: 0, y: 0 })];
+    this.costumes = [
+      new Costume("arrow", "./Arrow1/costumes/arrow.svg", { x: 0, y: 0 })
+    ];
 
     this.triggers = [
-      new Trigger(Trigger.KEY_PRESSED, { key: "space" }, this.shootArrow),
-      new Trigger(Trigger.CLONE_START, this.flyArrow)
+      new Trigger(Trigger.KEY_PRESSED, { key: "space" }, this.handleShoot),
+      new Trigger(Trigger.CLONE_START, this.whenCloneStarts)
     ];
   }
 
-  shootArrow() {
+  handleShoot() {
     this.createClone();
   }
 
-  flyArrow() {
-    this.goto(this.stage.vars.launcherX, this.stage.vars.launcherY); // Adjust if needed
+  *whenCloneStarts() {
     this.visible = true;
+
+    // Start where the shooter is (adjust this as needed)
+    this.goto(this.sprites["AppleCatcher"].x, this.sprites["AppleCatcher"].y);
+    this.direction = this.sprites["AppleCatcher"].direction;
+
+    // Fly until off-screen
     while (!this.touching("edge")) {
       this.move(10);
-      this.yield();
+      yield;
     }
+
     this.deleteThisClone();
   }
 }
